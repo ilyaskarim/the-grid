3b16b8d5419600752fad29bbb0d8907a470d4e7f
pervaizdattoo <pervaizdattoo99@gmail.com>
2019-10-22 17:28:56 -0700

    initial project setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,49 @@
-export default {
-    init() {
-        alert("initing")
-    },
-    options() {
-        return {
-            changing: 1
-        }
-    }
+
+let theGrid = function (opts) {
+	this.gridContainer = document.querySelector(opts.el)
+	this.columns = opts.columns;
+	this.data = opts.data;
+
+	this.generateBaseContainers = () => {
+		let columns = document.createElement("div");
+		columns.classList.add('columns');
+		let filters = document.createElement("div");
+		filters.classList.add('filters');
+		let data = document.createElement("div");
+		data.classList.add('data');
+		this.gridContainer.append(columns);
+		this.gridContainer.append(filters);
+		this.gridContainer.append(data);
+	}
+	this.generateColumns = function () {
+		this.columns.forEach((column) => {
+			let columnHTML = document.createElement("div");
+			columnHTML.innerHTML = column;			
+			columnHTML.classList.add('column');
+			this.gridContainer.querySelector(".columns").append(columnHTML);
+		});
+	}
+	this.generateData = function () {
+		this.data.forEach((userdata) => {
+			let row = document.createElement('div');
+			row.classList.add('row');
+			this.columns.forEach((column) => {
+				let userdataHTML = document.createElement("div");
+				userdataHTML.innerHTML = userdata[column];
+				userdataHTML.classList.add('userdata');
+				row.append(userdataHTML);
+			});
+			this.gridContainer.querySelector(".data").append(row);
+		});
+	}
+
+
+	this.init = function () {
+		this.generateBaseContainers();
+		this.generateColumns();
+		this.generateData();
+	}
+
+
+	this.init();
 }
